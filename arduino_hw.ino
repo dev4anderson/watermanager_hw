7454f1185a0d2e0d4c9c68de1aa803a8f974c00d
dev4anderson <nds.anderson@gmail.com>
2017-10-01 10:15:25 -0300

    Post API e organizando código

--- arduino_hw.ino
+++ arduino_hw.ino
@@ -5,10 +5,10 @@
 
 ESP8266WiFiMulti WiFiMulti;
 
-float vazao; //Variável para armazenar o valor em L/min
-float media=0; //Variável para tirar a média a cada 1 minuto
-int contaPulso; //Variável para a quantidade de pulsos
-int i=0; //Variável para contagem
+float vazao;      //Variável para armazenar o valor em L/min
+float media = 0;  //Variável para tirar a média a cada 1 minuto
+int contaPulso;   //Variável para a quantidade de pulsos
+int i = 0;        //Variável para contagem
 int contpulso;
 double litros;
 double cont;
@@ -17,13 +17,94 @@ String litrosStr;
 
 void setup() {
   
-    Serial.begin(9600);
-    delay(10);
+  configPorta();
 
-    pinMode(13, INPUT);
-    attachInterrupt(13, incpulso, RISING); //Configura o pino 2(Interrupção 0) para trabalhar como interrupção
+  conectaWifi();
 
-    // We start by connecting to a WiFi network
+}
+
+
+void loop() {
+  
+  calcLitros();
+
+  if(i==60){
+
+    enviaDados();
+
+    limpaLitros();
+    
+  }
+  
+}
+
+void incpulso ()
+{
+  contaPulso++; //Incrementa a variável de contagem dos pulsos
+  contpulso++;
+}
+
+void calcLitros ()
+{
+    contaPulso = 0;   //Zera a variável para contar os giros por segundos
+    sei();      //Habilita interrupção
+    delay (1000); //Aguarda 1 segundo
+    cli();      //Desabilita interrupção
+    cont = contpulso;
+    litros = (cont)/330;
+    vazao = contaPulso / 5.5; //Converte para L/min
+    media = media+vazao; //Soma a vazão para o calculo da media
+    i++;
+}
+
+void enviaDados ()
+{
+  if(WiFiMulti.run() == WL_CONNECTED){   //Check WiFi connection status
+ 
+     HTTPClient http;                     //Declare object of class HTTPClient
+
+     StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
+     JsonObject& JSONencoder = JSONbuffer.createObject(); 
+
+     litrosStr = litros;
+ 
+     JSONencoder["liter"] = litrosStr;
+     JSONencoder["serial"] = WiFi.macAddress();
+
+    
+     http.begin("https://watermanager.herokuapp.com/v1/consumption", "08:3B:71:72:02:43:6E:CA:ED:42:86:93:BA:7E:DF:81:C4:BC:62:30");      //Specify request destination
+     http.addHeader("Content-Type", "application/json");  //Specify content-type header
+
+     char JSONmessageBuffer[300];
+     JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
+     Serial.println(JSONmessageBuffer);
+ 
+     int httpCode = http.POST(JSONmessageBuffer);         //Send the request
+   
+     String payload = http.getString();                  //Get the response payload
+ 
+     Serial.println(httpCode);   //Print HTTP return code
+     Serial.println(payload);    //Print request response payload
+ 
+     http.end();  //Close connection 
+ 
+   }else{
+ 
+      Serial.println("Error in WiFi connection");   
+  }
+
+}
+
+void limpaLitros ()
+{
+    i = 0;
+    contaPulso = 0;
+    contpulso = 0;  
+}
+
+void conectaWifi ()
+{
+  // Conexão com rede Wifi
     WiFiMulti.addAP("******", "********");
 
     Serial.print("Wait for WiFi... ");
@@ -40,65 +121,14 @@ void setup() {
 
     delay(500);
 }
-
-
-void loop() {
-
-  contaPulso = 0;   //Zera a variável para contar os giros por segundos
-  sei();      //Habilita interrupção
-  delay (1000); //Aguarda 1 segundo
-  cli();      //Desabilita interrupção
-  cont = contpulso;
-  litros = (cont)/330;
-  vazao = contaPulso / 5.5; //Converte para L/min
-  media = media+vazao; //Soma a vazão para o calculo da media
-  i++;
-
-  if(i==2){
-
-if(WiFiMulti.run() == WL_CONNECTED){   //Check WiFi connection status
- 
-   HTTPClient http;                     //Declare object of class HTTPClient
-
-   StaticJsonBuffer<300> JSONbuffer;   //Declaring static JSON buffer
-   JsonObject& JSONencoder = JSONbuffer.createObject(); 
-
-    litrosStr = litros;
- 
-   JSONencoder["liter"] = litrosStr;
-   JSONencoder["serial"] = WiFi.macAddress();
-
-    
-   http.begin("https://watermanager.herokuapp.com/v1/consumption", "08:3B:71:72:02:43:6E:CA:ED:42:86:93:BA:7E:DF:81:C4:BC:62:30");      //Specify request destination
-   http.addHeader("Content-Type", "application/json");  //Specify content-type header
-
-   char JSONmessageBuffer[300];
-   JSONencoder.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-   Serial.println(JSONmessageBuffer);
- 
-   //int httpCode = http.POST(JSONmessageBuffer);         //Send the request
-   
-   String payload = http.getString();                  //Get the response payload
- 
-   //Serial.println(httpCode);   //Print HTTP return code
-   Serial.println(payload);    //Print request response payload
- 
-   http.end();  //Close connection 
- 
- }else{
- 
-    Serial.println("Error in WiFi connection");   
- 
- }
-
-  i = 0;
-  contaPulso = 0;
-  contpulso = 0;
-  }
-}
-
-void incpulso ()
+void configPorta ()
 {
-  contaPulso++; //Incrementa a variável de contagem dos pulsos
-  contpulso++;
+    Serial.begin(9600);
+    delay(10);
+
+    pinMode(13, INPUT);
+    attachInterrupt(13, incpulso, RISING); //Configura o pino 2(Interrupção 0) para trabalhar como interrupção
 }
+
+
+
